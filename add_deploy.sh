7546045d5e0d50d18b42e95d00437fd4e6c401f3
damonreeve <damon@ozoneproject.com>
2019-08-24 18:56:38 +0100

    added new deploy script

---
+++ add_deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# add any new content that's been created
+git add content/
+git commit content -m "new content added and updated"
+
+# rm the docs folder to prep for build
+rm -r docs
+mkdir docs
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd docs
+
+# Add changes to git.
+git add .
+
+# commit the new files and changes
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push
